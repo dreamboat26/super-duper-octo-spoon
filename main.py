426c8ad36ee5b5765ca918c040a84a263b3fe42a
Mahule Roy <125608791+dreamboat26@users.noreply.github.com>
2024-10-26 20:15:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from neo4j import GraphDatabase
 def main():
     st.set_page_config(
         layout="wide",
-        page_title="Graphy v1",
+        page_title="WID AI Automtaed Matching System",
         page_icon=":graph:"
     )
     st.sidebar.image('logo (2).png', use_column_width=True) 
@@ -26,7 +26,7 @@ def main():
     This application allows you to upload a PDF file, extract its content into a Neo4j graph database, and perform queries using natural language.
     It leverages LangChain and OpenAI's GPT models to generate Cypher queries that interact with the Neo4j database in real-time.
     """)
-    st.title("Graphy: Realtime GraphRAG App")
+    st.title("WID AI Automtaed Matching System App")
 
     load_dotenv()
 
